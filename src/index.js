0bf2f0ed9d35ef82cd2211be546cacabc42b76ae
ederpbj <ederpbj@yahoo.com.br>
2020-04-20 14:02:52 -0300

    Criando uma tela login em 10 min

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, {Component} from 'react';
-import {Text, StyleSheet, View, Image} from 'react-native';
+import {
+  Alert,
+  Text,
+  View,
+  Image,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+
+import styles from './styles';
 
 export default class index extends Component {
+  clicou = () => {
+    Alert.alert('CanalGeekDev', 'Você clicou no botão!');
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Image 
-          source={require('../../assets')}
+        <Image
+          // eslint-disable-next-line global-require
+          source={require('../assets/images/geekdev.jpg')}
+          style={styles.logo}
         />
+
+        <TextInput placeholder="Digite seu e-mail" style={styles.input} />
+
+        <TextInput
+          placeholder="Digite sua senha"
+          style={styles.input}
+          secureTextEntry
+        />
+
+        <TouchableOpacity
+          style={styles.botao}
+          onPress={() => {this.clicou()}}
+        >
+          <Text style={styles.botaoText}>Login</Text>
+        </TouchableOpacity>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({});
--- src/styles/index.js
+++ src/styles/index.js
@@ -1,9 +1,10 @@
+/* eslint-disable prettier/prettier */
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#0066cc',
+    backgroundColor: '#2c3e50',
     justifyContent: 'center',
     alignItems: 'center',
   },
@@ -15,9 +16,34 @@ const styles = StyleSheet.create({
     color: '#000',
   },
   botao: {
-    borderBottomColor: '#000',
-    backgroundColor: '#f656',
+    width: 300,
+    height: 42,
+    backgroundColor: '#3498db',
+    marginTop: 10,
+    borderRadius: 4,
+    alignItems: 'center',
+    justifyContent: 'center'
+    
   },
+  botaoText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#fff'
+  },
+  logo: {
+    width: 150,
+    height: 150,
+    borderRadius: 100
+  },
+  input: {
+    marginTop: 10,
+    padding: 10,
+    width: 300,
+    backgroundColor: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+    borderRadius: 3
+  }
 });
 
 export default styles;
